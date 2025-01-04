9a98ad270747c3929ff15035ba18316f9cd887b6
Santiago Moreno <santiago.moreno.dev01@gmail.com>
2025-01-04 17:57:17 -0500

    Inicio del proyecto

---
+++ src/test/java/com/santiago/api/task/TaskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.api.task.TaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
